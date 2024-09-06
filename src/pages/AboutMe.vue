cdb74193279ca7e2c22b3fafbc99bf025eb08775
Ricardo Garcia <ricardolacadgarcia@gmail.com>
2024-09-06 13:11:54 +0800

    updates

--- src/pages/AboutMe.vue
+++ src/pages/AboutMe.vue
@@ -5,7 +5,7 @@
             <h1 class="about-me-title">About Me</h1>
             <p class="about-me-p">
                 Hello! <span class="highlight">I'm Ricardo L. Garcia</span>, a 23-year-old computer science professional from Quezon City, Philippines. I hold a Bachelor of Science in Computer Science from the Eulogio "Amang" Rodriguez Institute of Science and Technology (EARIST).
-                With a strong passion for innovation and problem-solving, I specialize in <span class="underline">Web and App Design</span>, <span class="underline">UI/UX Design</span>, <span class="underline">Graphic Design</span>, and <span class="underline">Front-End Development</span>. I am committed to leveraging my skills to create engaging and effective digital experiences. I am willing to be train and enhance my tech skills. Welcome to my portfolio!
+                With a strong passion for innovation and problem-solving, I specialize in <span class="underline">Web and App Design</span>, <span class="underline">UI/UX</span>, <span class="underline">Graphic Design</span>, and <span class="underline">Front-End Development</span>. I am committed to leveraging my skills to create engaging and effective digital experiences. I am willing to be train and enhance my tech skills. Welcome to my portfolio!
             </p>
             <div class="about-me-action-links">
                 <a class="about-me-action-link" href="mailto:ricardolacadgarcia@gmail.com">Email Me <img class="arrow-up-right-icon" src="../assets/icons/arrow-up-right-icon.svg" alt="arrow-up-right-icon"></a>
